4124c1319a28348b2b8689e2e268e5209ba84ad1
Ivan Antonov <ivan1antonov@gmail.com>
2024-10-03 08:14:24 +0400

    solution task copy fs

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,20 @@
+import fs from 'fs/promises';
 const copy = async () => {
-    // Write your code here 
+  const source = './files';
+  const dest = './files_copy';
+      try{
+        const srcStat = await fs.stat(source);
+        if(!srcStat.isDirectory()) throw new Error("Source is not Directory");
+        const destExists = await fs.access(dest).then(() => true).catch(() => false);
+        if(destExists){
+          throw new Error("FS operation failed"); 
+        }
+        await fs.cp(source, dest, { recursive: true });
+        console.log('Directory was successfully copied!');
+      }catch(err){
+        console.error("Error occurred:", err.message);
+        throw new Error("FS operation failed: " + err.message);      
+      }
 };
 
 await copy();
