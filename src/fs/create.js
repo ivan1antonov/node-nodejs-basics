4fa1ee55f0571cdcb584dc0efacfb3ca89e5e534
Ivan Antonov <ivan1antonov@gmail.com>
2024-10-03 10:26:37 +0400

    feat: add solution task create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
 const create = async () => {
-    // Write your code here 
+  const fs = require('node:fs/promises');
+  const path = './files/fresh.txt';
+  const fileExist = async () => {
+    try {
+      await fs.stat(path);
+      return true;
+    } catch (err) {
+      return false;
+    }
+  };
+  let exist = await fileExist();
+  if (!exist) {
+    await fs.writeFile(path, 'I am fresh and young');
+    console.log('File was create');
+  } else {
+    throw new Error('FS operation failed');
+  }
 };
 
 await create();
