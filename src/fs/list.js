d8ead8f0d6a3eb329fdee55580049ad00b559ee8
Ivan Antonov <ivan1antonov@gmail.com>
2024-10-04 10:37:24 +0400

    feat: add solution task list js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import fs from 'node:fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'node:url';
+
 const list = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const directory = path.join(__dirname, 'files');
+
+  console.log('__filename:', __filename);
+  console.log('__dirname:', __dirname);
+  console.log('directory:', directory);
+
+  try {
+    const readDir = await fs.readdir(directory);
+    console.log('files:', readDir);
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await list();
