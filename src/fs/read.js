b93828ec12d84f6ad6eeacbeae75a2a50671cd81
Ivan Antonov <ivan1antonov@gmail.com>
2024-10-04 10:52:04 +0400

    feat: add solution task read js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const currentFile = path.join(__dirname, 'files', 'fileToRead.txt');
+
+  try {
+    const readFile = await fs.readFile(currentFile, { encoding: 'utf-8' });
+    console.log(readFile);
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await read();
