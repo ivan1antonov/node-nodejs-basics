5f066706f659b70baea64529c8dfcf267fcc0cc2
Ivan Antonov <ivan1antonov@gmail.com>
2024-10-05 19:45:27 +0400

    feat: add solution task env js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envVar = process.env;
+
+    const rssVar = Object.entries(envVar);
+
+    const resVar = rssVar
+      .filter( ([key]) => key.startsWith("RSS_"))
+      .map(([key, value]) => `${key}=${value}`)
+      .join('; ');
+
+    console.log(resVar);
+    return resVar;
 };
 
 parseEnv();
