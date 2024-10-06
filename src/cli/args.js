2650abdbb8d12eb0f97e779934920f063352a6eb
Ivan Antonov <ivan1antonov@gmail.com>
2024-10-06 17:35:26 +0400

    feat: add solution env js and args js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+    const variable = process.argv.slice(2);
+    let res =[];
+    variable.forEach( (el, index) => {
+        if(el.startsWith('--')){
+          res.push(`${el.substring(2)} is ${variable[index+1]}`);
+        }
+    });
+    console.log(res.join(', '));
+    return res;
 };
 
 parseArgs();
