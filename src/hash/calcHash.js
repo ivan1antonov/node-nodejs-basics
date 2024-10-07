dcdc45803a7c0b04c13a3dbb132b71fff961468d
Ivan Antonov <ivan1antonov@gmail.com>
2024-10-07 08:11:54 +0400

    feat: add solution hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,30 @@
+import { createHash } from 'crypto';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const fileToHash = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+    const fileStream = fs.createReadStream(fileToHash);
+
+    const hash = createHash('sha256');
+
+    fileStream.on('data', (chunk) => {
+      hash.update(chunk);
+    });
+
+    fileStream.on('end', () => {
+      const resultHash = hash.digest('hex');
+      console.log(resultHash);
+    })
+
+    fileStream.on('error', (err) => {
+      console.error(err);
+    })
 };
 
 await calculateHash();
