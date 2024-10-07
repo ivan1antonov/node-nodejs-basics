119c474176a88fd3c6d5efe967d652f678b3da3d
Ivan Antonov <ivan1antonov@gmail.com>
2024-10-07 23:27:44 +0400

    feat: add last tasks

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,27 @@
+import { spawn } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = spawn('node', ['./files/script.js', ...args], {
+    stdio: ['pipe', 'pipe', 'inherit']
+  });
+
+  process.stdin.pipe(child.stdin);
+  process.stdin.on('end', () => {
+    child.stdin.end();
+  });
+  child.stdout.pipe(process.stdout);
+
+  child.on('error', (error) => {
+    console.error(`Error into child process: ${error.message}`);
+  });
+
+  child.on('exit', (code) => {
+    console.log(`Child process finished with code: ${code}`);
+  });
+  child.on('exit', (code) => {
+    process.exit(code);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg1', 'arg2']);
